78cd9308795859eec44d6b6de47ef521bf3d6474
HCVincent <lilacsweet@163.com>
2024-12-20 17:18:47 +0800

    Custom Editor Icons For Menu Entries

--- Plugins/SuperManager/Source/SuperManager/Private/CustomStyle/SuperManagerStyle.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/CustomStyle/SuperManagerStyle.cpp
@@ -32,6 +32,11 @@ TSharedRef<FSlateStyleSet> FSuperManagerStyle::CreateSlateStyleSet()
 		new FSlateImageBrush(IconDirectory / "DeleteEmptyFolders.png", Icon16x16));
 	CustomStyleSet->Set("ContentBrowser.AdvanceDeletion",
 		new FSlateImageBrush(IconDirectory / "AdvanceDeletion.png", Icon16x16));
+
+	CustomStyleSet->Set("LevelEditor.LockSelection",
+		new FSlateImageBrush(IconDirectory / "SelectionLock.png", Icon16x16));
+	CustomStyleSet->Set("LevelEditor.UnlockSelection",
+		new FSlateImageBrush(IconDirectory / "SelectionUnlock.png", Icon16x16));
 	return CustomStyleSet;
 }
 
